4cf088a17060df0f27cbb32b79606c0e056fefb8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-08 14:43:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 3 exercice 7</title>
+</head>
+<body>
+    <?php
+        for($i = 1; $i <= 100; $i= $i + 15){
+            echo '<p> On tient le bon bout. Déja ' . $i . ' pas< /p>';
+        }
+    ?>
+</body>
+</html>
